e0cd6fccfca80a768fa6059315e0f943d24914cd
Daniel Gutiérrez <d.xander.gutierrez@gmail.com>
2024-09-30 08:50:18 -0600

    Sesion 13

---
+++ Burbuja sesion13/person/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace person
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.dgvPersons = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(50, 156);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(177, 155);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 3;
+            this.btnSort.Text = "Ordenar";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha de nacimiento:";
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Location = new System.Drawing.Point(191, 119);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(200, 22);
+            this.dtpBirthDate.TabIndex = 5;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(128, 28);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(216, 22);
+            this.tbName.TabIndex = 6;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(128, 67);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(216, 22);
+            this.tbLastName.TabIndex = 7;
+            // 
+            // dgvPersons
+            // 
+            this.dgvPersons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersons.Location = new System.Drawing.Point(50, 203);
+            this.dgvPersons.Name = "dgvPersons";
+            this.dgvPersons.RowHeadersWidth = 51;
+            this.dgvPersons.RowTemplate.Height = 24;
+            this.dgvPersons.Size = new System.Drawing.Size(341, 181);
+            this.dgvPersons.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvPersons);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.dtpBirthDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.DataGridView dgvPersons;
+    }
+}
+
---
+++ Burbuja sesion13/person/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using person.models;
+
+namespace person
+{
+    public partial class Form1 : Form
+    {
+        List<Person> persons;
+        public Form1()
+        {
+            InitializeComponent();
+            persons = new List<Person>();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Person person = new Person();
+            person.firstName = tbName.Text;
+            person.lastName = tbLastName.Text;
+            person.birhtdate = dtpBirthDate.Value;
+
+            persons.Add(person);
+            ShowPersons();
+            ClearTb();
+
+        }
+        private void ShowPersons()
+        {
+            dgvPersons.DataSource = null;
+            dgvPersons.DataSource = persons;
+        }
+        private void ClearTb()
+        {
+            tbName.Clear();
+            tbLastName.Clear();
+            dtpBirthDate.Value = DateTime.Now;
+            tbName.Focus();
+        }
+    }
+}
