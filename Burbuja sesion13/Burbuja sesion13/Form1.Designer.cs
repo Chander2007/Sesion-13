73bc594ed4d7330e2d0be84ae859f7759d20d581
Daniel Gutiérrez <d.xander.gutierrez@gmail.com>
2024-09-30 08:16:30 -0600

    Sesion13

---
+++ Burbuja sesion13/Burbuja sesion13/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Burbuja_sesion13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un #:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(306, 27);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(111, 26);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(122, 27);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(165, 22);
+            this.tbNumber.TabIndex = 2;
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.ItemHeight = 16;
+            this.lbNumbers.Location = new System.Drawing.Point(122, 71);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(165, 244);
+            this.lbNumbers.TabIndex = 3;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(306, 71);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(111, 23);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Ordenar";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.lbNumbers);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ Burbuja sesion13/Burbuja sesion13/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Burbuja_sesion13.operation;
+namespace Burbuja_sesion13
+{
+    public partial class Form1 : Form
+    {
+        int[] numbers = new int[10];
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(tbNumber.Text);
+            numbers[i++] = number;
+            showNumbers();
+        }
+
+        private void showNumbers()
+        {
+            if(numbers.Length > 0)
+            {
+                lbNumbers.Items.Clear();
+            }
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                lbNumbers.Items.Add(numbers[i]);
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            Bubble burbuja = new Bubble();
+            burbuja.ordenarBurbuja(numbers);
+            showNumbers();
+        }
+    }
+}
